d013ad21434a4e429c8b455d275234c32f768d1a
mladen5000 <mladenrasic@mladen-rasics-macbook.local>
2015-01-07 14:16:00 -0600

    Added a preliminary howto section

--- flory.py
+++ flory.py
@@ -22,6 +22,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/howto.html')
+def howto():
+	return render_template("howto.html")
+
 @app.route('/Flory.html',methods=['POST','GET'])
 def flory():
 	return render_template("Flory.html")
@@ -42,29 +46,34 @@ def slctplot():
 		polya = request.form['polya']
 		polyb = request.form['polyb']
 		z = 6.0
+		
+		""" Parameters for specific polymers"""
+		""" Should encapsulate this elsewhere eventually """
 
-		if polya == "poly1":
+		if polya == "PH1":
 				r1 = 1.150
 				p1 = 1.333
-		if polya == "poly2":
+		if polya == "PEP":
 				r1 = 1.200
 				p1 = 1.200
-		if polya == "poly3":
+		if polya == "PIB":
 				r1 = 1.750
 				p1 = 1.500
 
-		if polyb == "poly1":
+		if polyb == "PH1":
 				r2 = 1.150
 				p2 = 1.333
-		if polyb == "poly2":
+		if polyb == "PEP":
 				r2 = 1.200
 				p2 = 1.200
-		if polyb == "poly3":
+		if polyb == "PIB":
 				r2 = 1.750
 				p2 = 1.500
 
 		print r1, r2, z, p1, p2, na, nb
+		global flipper
 
+		"""Nice trick to fix asymmetry issue"""
 		if na > nb:
 				print "we gotta flip!"
 				flipper = 1
@@ -73,6 +82,7 @@ def slctplot():
 		else:
 				flipper = 0
 
+		"""Set up the plot"""
 		fig = Figure()
 		fig.set_facecolor('white')
 		axis = fig.add_subplot(1, 1, 1,axisbg='#f5f5f5')
@@ -80,11 +90,16 @@ def slctplot():
 		axis.set_ylabel('eps/kbT')
 		axis.set_title('SLCT Phase Diagram')
 
-		phi, y2 = SLCT_NR(flipper,r1,r2,z,p1,p2,na,nb)
-		spinx,spiny = SLCT_Spinodal(flipper,r1,r2,z,p1,p2,na,nb)
+		"""Run Optimization"""
+		phi, y2 = SLCT_NR(r1,r2,z,p1,p2,na,nb)
+		spinx,spiny = SLCT_Spinodal(r1,r2,z,p1,p2,na,nb)
+
+
+		"""Plot"""
 		spinline = axis.plot(spinx,spiny,'r',lw=2) 
 		binline = axis.plot(phi,y2,'b',lw=2)
 
+		"""Add d3 stuff"""
 		canvas = FigureCanvas(fig)
 		output = StringIO.StringIO()
 		canvas.print_png(output, bbox_inches='tight')
@@ -95,13 +110,17 @@ def slctplot():
 	
 @app.route('/plot', methods=['GET','POST'])	
 def login():
+	
 	if request.method == 'POST':
 		na = float(request.form['NFA'])
 		nb = float(request.form['NFB'])
 
+		"""Spinodal"""
 		crit_chi = .5*((1/(na**.5) + 1/(nb**.5))**2)
 		nav = 2./crit_chi
+		global flipper
 
+		"""Flipper"""
 		if na > nb:
 				print "we gotta flip!"
 				flipper = 1
@@ -111,86 +130,124 @@ def login():
 				flipper = 0
 
  
+		"""Set up the plot"""
 		fig = Figure()
 		fig.set_facecolor('white')
 		axis = fig.add_subplot(1, 1, 1,axisbg='#f5f5f5')
 		axis.set_xlabel('Volume Fraction')
 		axis.set_ylabel('Chi')
 		axis.set_title('Flory-Huggins Phase Diagram')
+
+		"""Run Optimization"""
+		"""Need to move these lines it's own function"""
 		x = arange(0.05,0.95,0.001)
 		spinodal = nav*(.5*(1./(na*x) + 1./(nb-nb*x)))
+
 		if flipper == 1:
 			x = 1 - x
 
-		phi,y2 =  NR(flipper,na,nb,nav,crit_chi)
+		phi,y2 =  NR(na,nb,nav,crit_chi)
 		spinline = axis.plot(x,spinodal,'r',lw=2) 
 		binline = axis.plot(phi,y2,'b',lw=2)
+
+		"""Add d3 stuff"""
 		canvas = FigureCanvas(fig)
 		output = StringIO.StringIO()
 		canvas.print_png(output, bbox_inches='tight')
 		plugins.connect(fig, plugins.MousePosition())
 
-#		json01 = json.dumps(mpld3.fig_to_dict(fig,template_type='simple'))
 		return mpld3.fig_to_html(fig,template_type='simple')
-#		return render_template("plot.html",json01=json01)
 
 @app.route('/vornplot', methods=['GET','POST'])	
 def vornplot():
 	if request.method == 'POST':
 		N = float(request.form['N'])
+
+		"""Set up the plot"""
 		fig = Figure()
 		fig.set_facecolor('white')
 		axis = fig.add_subplot(1, 1, 1,axisbg='#f5f5f5')
-		x = arange(1e-5,0.1,0.0001)
- 		spinodal = ((2 * (2**.333) * ((N*x -x +1)**.666))/((3**.666)*(alpha**.666)*(N**.666)*(((x-1)**2)**(1./3.))*(x**.333)))
-		phi,y2 =  vNR(alpha,N)
-		line1 = axis.plot(x,spinodal,'r',lw=2)
-		spinline = axis.plot(phi,y2,'b',lw=2) 
 		axis.set_xlabel('Volume Fraction')
 		axis.set_ylabel('Charge Density')
 		axis.set_title('Voorn-Overbeek Phase Diagram')
 		canvas = FigureCanvas(fig)
-#		output = StringIO.StringIO()
-#		canvas.print_png(output, bbox_inches='tight')
+
+		"""Move Spinodal Elsewhere"""
+		phi,y2 =  vNR(alpha,N)
+		x, spinodal = vorn_Spinodal(alpha,N)
+		line1 = axis.plot(x,spinodal,'r',lw=2)
+		spinline = axis.plot(phi,y2,'b',lw=2) 
+
+		"""Make this organized like the other stuff"""
 		plugins.connect(fig, plugins.MousePosition())
 		return mpld3.fig_to_html(fig)
 
-
+		
 
 
 def flip(a1,a2,b1,b2,c1,c2):
+		""" Switch a1 with a2, b1 with b2, c1 with c2"""
 		return a2,a1,b2,b1,c2,c1
 
 
 """ Voorn-Overbeek """
 
+def vorn_Spinodal(alpha,N):
+		x = arange(1e-5,0.1,0.0001)
+ 		spinodal = ((2 * (2**.333) * ((N*x -x +1)**.666))/((3**.666)*(alpha**.666)*(N**.666)*(((x-1)**2)**(1./3.))*(x**.333)))
+		return x, spinodal
+
 def vfun(x,alpha,N,phi1):
 	"F1 = f'(phi_1a) - f'(phi_2a); F2 = (b-a)*f'(phi_1a) -[ f(phi_2a) - f(phi_1a) ]"
-	return array([1.5*alpha*x[1]*(x[1]*phi1)**0.5 - 1.5*alpha*x[1]*(x[1]*x[0])**0.5
+	return array([
+
+			1.5*alpha*x[1]*(x[1]*phi1)**0.5 - 1.5*alpha*x[1]*(x[1]*x[0])**0.5
 			- log(phi1/2.)/N + log(x[0]/2.)/N + log(1-phi1) - log(1-x[0]),
-			-1.5*alpha*x[1]*x[0]*(x[1]*phi1)**.5 + .5*alpha*x[1]*phi1*(x[1]*phi1)**.5 + alpha*x[1]*x[0]*(x[1]*x[0])**.5 + x[0]*log(phi1/2)/N - phi1/N + x[0]/N - x[0]*log(x[0]/2)/N - x[0]*log(1-phi1) + phi1 + log(1-phi1) - x[0] + x[0]*log(1-x[0]) - log(1-x[0])])
+
+			-1.5*alpha*x[1]*x[0]*(x[1]*phi1)**.5 + .5*alpha*x[1]*phi1*(x[1]*phi1)**.5 
+			+ alpha*x[1]*x[0]*(x[1]*x[0])**.5 + x[0]*log(phi1/2)/N - phi1/N 
+			+ x[0]/N - x[0]*log(x[0]/2)/N - x[0]*log(1-phi1) + phi1 
+			+ log(1-phi1) - x[0] + x[0]*log(1-x[0]) - log(1-x[0])
+
+			])
 
 def vjac(x,alpha,N,phi1):
 	"df1/dphi2, df1/dchi; df2/dphi2, df2/dchi"
-	return array([[((-3.*alpha*x[1]**2.)/(4.*(x[1]*x[0])**0.5)) + 1./(N*x[0]) + 1./(1.-x[0]), # dF1/dphi2
-		 2.25*alpha*((x[1]*phi1)**0.5 - (x[1]*x[0])**0.5)], #dF1/dsigma
-		 [-1.5*alpha*x[1]*(x[1]*phi1)**0.5 + 1.5*alpha*x[1]*(x[1]*x[0])**0.5 + log(phi1)/N - log(x[0])/N - log(1.-phi1) + log(1.-x[0]), #dF2/dphi2
-			0.75*alpha*(-3.*x[0]*(x[1]*phi1)**.5 + phi1*(x[1]*phi1)**.5 + 2.*x[0]*(x[1]*x[0])**.5)]]) #dF2/dsigma
+	return array([[
+
+			((-3.*alpha*x[1]**2.)/(4.*(x[1]*x[0])**0.5)) 
+			+ 1./(N*x[0]) + 1./(1.-x[0]), # dF1/dphi2
+
+		 	2.25*alpha*((x[1]*phi1)**0.5 - (x[1]*x[0])**0.5)], #dF1/dsigma
+
+		 	[
+			-1.5*alpha*x[1]*(x[1]*phi1)**0.5 + 1.5*alpha*x[1]*(x[1]*x[0])**0.5 
+			+ log(phi1)/N - log(x[0])/N - log(1.-phi1) + log(1.-x[0]), #dF2/dphi2
+
+			0.75*alpha*(-3.*x[0]*(x[1]*phi1)**.5 + phi1*(x[1]*phi1)**.5 
+			+ 2.*x[0]*(x[1]*x[0])**.5)
+
+			]]) #dF2/dsigma
+
+def v_crit(alpha,N):
+		crit_phi = (-(N+2) + sqrt((N+2)**2 + 4*(N-1)))/(2*(N-1))
+		crit_phi = crit_phi - .0001
+		return crit_phi
+
 
 def vNR(alpha,N):
 		" Newton Raphson solver for the binary mixture"
 		# Set up parameters, initial guesses, formatting, initializing etc.
-		crit_phi = (-(N+2) + sqrt((N+2)**2 + 4*(N-1)))/(2*(N-1))
-		crit_phi = crit_phi - .0001
+		crit_phi = v_crit(alpha,N)
+
 		phi1vals = arange(2e-7,crit_phi,.0001)
 		phi1vals = phi1vals.tolist()
 		guess = [0,0]
 		new_guess = [0.9,.9] #phi2, sigma
 		iter = 0
-		length = len(phi1vals)
-		y2 = zeros((length,1))
-		x2 = zeros((length,1))
-		x1 = zeros((length,1))
+		y2 = zeros((len(phi1vals),1))
+		x2 = zeros((len(phi1vals),1))
+		x1 = zeros((len(phi1vals),1))
 		max_iter = 2000
 
 		#Loop to find the roots using Multivariate Newton-Rhapson
@@ -231,20 +288,7 @@ def vNR(alpha,N):
 
 def fun(x,na,nb,phi1):
 	"F1 = f'(phi_1a) - f'(phi_2a); F2 = (b-a)*f'(phi_1a) -[ f(phi_2a) - f(phi_1a) ]"
-	"""na and nb are equivalent to m1, m2
 
-			(x[0] - phi1)*(1./na - 1./nb + x[1] - 2*x[1]*phi1
-			- log(1-phi1)/nb + log(phi1)/na) - ((x[0]/na)*log(x[0])
-			+ ((1-x[0])/nb)*log(1-x[0]) + x[1]*x[0]*(1-x[0]))
-			+ ((phi1/na)*log(phi1) + ((1-phi1)/nb)*log(1-phi1) + x[1]*(phi1)*(1-phi1))
-			])	
-
-
-			-x[0] - x[1]*nb*x[0] + (nb/na)*x[0] + x[1]*nb*(1-x[0])*x[0] 
-			+ phi1 + x[1]*nb*phi1 - (nb/na)*phi1 - x[1]*nb*(1-phi1)*phi1 
-			- log(1-x[0]) + log(1-phi1)])
-
-	"""
 	na = 1.0*na
 	nb = 1.0*nb
 	return array([ 
@@ -264,39 +308,18 @@ def fun(x,na,nb,phi1):
 
 def jac(x,na,nb,phi1):
 	"df1/dphi2, df1/dchi; df2/dphi2, df2/dchi"
-	"""
-			[
-			log(phi1)/na -log(x[0])/na - log(1-phi1)/nb + log(1-x[0])/nb
-			-2*x[1]*phi1 + 2*x[1]*x[0],
-
-			(x[0] - phi1)**2
-			]])
-
-			[-1 - x[1]*nb + nb/na + 1.0/(1-x[0]) + x[1]*nb*(1-x[0]) - x[1]*nb*x[0],
-
-			nb*phi1 - nb*(1-phi1)*phi1 - nb*x[0] + nb*(1-x[0])*x[0]]])
-
-	"""
 	na = 1.0*na
 	nb = 1.0*nb
-	return array([[
-			1 + x[1]*na - na/nb + x[1]*na*(1-x[0]) - 1.0/x[0] - x[1]*na*x[0],
 
+	"Make me better looking somehow"
+	return array([
+			[1 + x[1]*na - na/nb + x[1]*na*(1-x[0]) - 1.0/x[0] - x[1]*na*x[0],
 			na*(1-phi1) - na*(1-phi1)*phi1 - na*(1-x[0]) + na*(1-x[0])*(x[0])],
-
-			[
-			log(phi1)/na -log(x[0])/na - log(1-phi1)/nb + log(1-x[0])/nb
-			-2*x[1]*phi1 + 2*x[1]*x[0],
-
-			(x[0] - phi1)**2
-			]])
+			[log(phi1)/na -log(x[0])/na - log(1-phi1)/nb + log(1-x[0])/nb
+			- 2*x[1]*phi1 + 2*x[1]*x[0], (x[0] - phi1)**2]])
 
 
-
-
-
-
-def NR(flipper,na,nb,nav,crit_chi):
+def NR(na,nb,nav,crit_chi):
 		" Newton Raphson solver for the binary mixture"
 		# Set up parameters, initial guesses, formatting, initializing etc.
 
@@ -310,10 +333,10 @@ def NR(flipper,na,nb,nav,crit_chi):
 		guess = [0,0]
 		new_guess = [0.5,3]
 		iter = 0
-		length = len(phi1vals)
-		y2 = zeros((length,1))
-		x2 = zeros((length,1))
-		x1 = zeros((length,1))
+
+		x1 = zeros((len(phi1vals),1))
+		x2 = zeros((len(phi1vals),1))
+		y2 = zeros((len(phi1vals),1))
 		max_iter = 2000
 
 		#Loop to find the roots using Multivariate Newton-Rhapson
@@ -332,21 +355,27 @@ def NR(flipper,na,nb,nav,crit_chi):
 					x2[index] = new_guess[0]
 					y2[index] = new_guess[1]
 					break
-		#Convert Numpy arrays (x1,x2,y2) to a list
-		n = size(x1) + 1
-		x1 = reshape(append(x1,crit_phi),(n,1))
+
+		# Flips the function back
 		if flipper ==1:
 			x1 = 1 - x1
 			x2 = 1 - x2
+
+		n = size(x1) + 1
+		x1 = reshape(append(x1,crit_phi),(n,1))
 		x1=x1.tolist()
 		x2=x2.tolist()
 
-		x2=x2[::-1] #Has to reverse the order of x2, which was converted to a tuple in the previous line
+		#Has to reverse the order of x2, which was converted to a tuple in the previous line
+		x2=x2[::-1] 
+
+		#Adds crit chi to the end of y2
 		y2 = reshape(append(y2,crit_chi),(n,1))
 		y2 = nav*y2
 		y2=y2.tolist()
 		y2i = y2[::-1]
 		y2i.pop(0)
+
 		#Concatenate the lists together
 		phi = x1 + x2
 		y2 = y2 + y2i
@@ -369,27 +398,28 @@ def SLCT_crit(r1,r2,z,p1,p2,na,nb):
 		phi_c_temp =  roots(coeff)
 
 
+		"Make sure that you pick the root that is real, positive and bounded by 0 and 1"
 		for critval in phi_c_temp:
 			if critval > 0 and critval < 1 and critval.imag == 0:
 				phi_c = critval.real
+
+		"Calculate the critical temperature"
 		Tc = 2*(b + c*phi_c)/(1.0/(m*phi_c) + 1.0/(m*k*(1-phi_c)) - 2*a)
 		return phi_c, Tc
 
 
 
-def SLCT_Spinodal(flipper,r1,r2,z,p1,p2,na,nb):
-		#put all these values into a list
+def SLCT_Spinodal(r1,r2,z,p1,p2,na,nb):
 		phi = arange(0.01,.99,0.001)
 		a = (r1 - r2)**2 / z**2
-		b =((z-2)/2 + (1/z)*(-2*p1 + p2)) #Technically this is b/(eps/kt) which is factored out
-		c = (3/z)*(p1 - p2) #Technically c / (eps/kt) 
+		b =((z-2)/2 + (1.0/z)*(-2*p1 + p2)) #Technically this is b/(eps/kt) which is factored out
+		c = (3.0/z)*(p1 - p2) #Technically c / (eps/kt) 
 		f = (.5*(1./(na*phi) + 1./(nb-nb*phi)))
 		spin1 = (f - a) / (b + c*phi)
-		btmspin = (z - 2 + (2./z)*(p1*(1 - 3*(1-phi)) + p2*(1-3*phi)))
-		topspin = (1./(na*phi) + 1./(nb*(1-phi)) - 2*a)
-		spin2 = topspin/btmspin
+
 		if flipper == 1:
 				phi = 1 - phi
+
 		return phi,spin1
 
 def SLCT_fun(x,phi1,r1,r2,z,p1,p2,na,nb):
@@ -459,7 +489,7 @@ def SLCT_jac(x,phi1,r1,r2,z,p1,p2,na,nb):
 """insert df2/dphi'' and df2/db here """
 
 
-def SLCT_NR(flipper,r1,r2,z,p1,p2,na,nb):
+def SLCT_NR(r1,r2,z,p1,p2,na,nb):
 		" Newton Raphson solver for the binary mixture"
 		# Set up parameters, initial guesses, formatting, initializing etc.
 		phi_c, Tc = SLCT_crit(r1,r2,z,p1,p2,na,nb)
@@ -477,17 +507,12 @@ def SLCT_NR(flipper,r1,r2,z,p1,p2,na,nb):
 		#Loop to find the roots using Multivariate Newton-Rhapson
 		for phi in phi1vals:
 			iter = 0
-			damp = 1.0
+			damp = 0.5
 			while iter < max_iter :
 
 				iter += 1
 				index = phi1vals.index(phi)
 				guess = new_guess
-				if guess[0] < 0 or guess[0] > 1:
-						guess[0] = random.random()
-						guess[1] = 0
-						damp = damp/2.0
-						print phi, iter, damp, guess[0]
 				jacobian = SLCT_jac(guess,phi,r1,r2,z,p1,p2,na,nb)
 				invjac = inv(jacobian)
 				invjac = inv(jacobian)
@@ -512,27 +537,6 @@ def SLCT_NR(flipper,r1,r2,z,p1,p2,na,nb):
 		y2 = y2 + y2i
 
 		return (phi,y2)
-		"""
-		n =  size(x1) + 1
-		x1 = reshape(append(x1,phi_c),(n,1))
-		
-		x1=x1.tolist()
-		x2=x2.tolist()
-
-		x2=x2[::-1] #Has to reverse the order of x2, which was converted to a tuple in the previous line
-		eps_c = 0.1/Tc
-		y2 = reshape(append(y2,eps_c),(n,1))
-
-		#y2 = nav*y2
-		y2=y2.tolist()
-		y2i = y2[::-1]
-		y2i.pop(0)
-		#Concatenate the lists together
-		phi = x1 + x2
-		y2 = y2 + y2i
-		return (phi,y2)
-
-		"""
 
 na = 1
 nb = 1
@@ -540,14 +544,6 @@ crit_chi = .5
 crit_phi = 1
 alpha = 3.655
 N = 1
-"""
-phi, y2 = SLCT_NR(  1.2,1.2,6,1.2,1.2,300,2000)
-phix,spinx2 = SLCT_Spinodal(1.2,1.2,6,1.2,1.2,300,2000)
-plt.plot(phi,y2)
-plt.plot(phix,spinx2)
-
-plt.show()
-"""
 
 
 
