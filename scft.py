b57ba1329b75f3a7216cec70c25fe325582a0be5
mladen5000 <mladenrasic@wireless-s1-no-150-20-243.uchicago.edu>
2015-10-07 16:39:26 -0500

    changed from check_output to call

--- scft.py
+++ scft.py
@@ -6,7 +6,7 @@ def hello_world():
 
 	os.chdir('/app/SCFT_real')
 	subprocess.call(['make'])
-	output = subprocess.check_output(['./rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
+	output = subprocess.call(['./rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
 	#output = subprocess.check_output(['./rscft'])
 	#print output
 	#output = subprocess.check_output(['./SCFT_real/rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
