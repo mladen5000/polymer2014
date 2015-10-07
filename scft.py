decbf173d611d5b41a22a0eb9d67ce45bf6f418c
mladen5000 <mladenrasic@wireless-s1-no-150-20-243.uchicago.edu>
2015-10-07 16:35:58 -0500

    makefile

--- scft.py
+++ scft.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import subprocess
+import os
 
 def hello_world():
 
-	subprocess.call(['make','-f','SCFT_real/Makefile'])
-	output = subprocess.call(['./rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
+	os.chdir('/app/SCFT_real')
+	subprocess.call(['make'])
+	output = subprocess.check_output(['./rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
 	#output = subprocess.check_output(['./rscft'])
 	#print output
 	#output = subprocess.check_output(['./SCFT_real/rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
