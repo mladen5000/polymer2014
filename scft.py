dd8a04f6b18eec7092528506b58449ef93e27ebe
mladen5000 <mladenrasic@wireless-s1-no-150-20-243.uchicago.edu>
2015-10-07 17:49:28 -0500

    changed scft.py for remote

--- scft.py
+++ scft.py
@@ -5,13 +5,12 @@ import os
 def hello_world():
 
 
-	#os.chdir('/app/SCFT_real')
-	#subprocess.call(['make'])
-	output = subprocess.call(['./LOCAL_rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
-	#output = subprocess.check_output(['./rscft'])
-	#print output
-	#output = subprocess.check_output(['./SCFT_real/rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
+	#These 3 lines are for remote
+	os.chdir('/app/SCFT_real')
+	subprocess.call(['make'])
+	output = subprocess.call(['./rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
 
-	""" Test Line """
-	#output = subprocess.check_output('ls')
+	# This line is for local only
+	#output = subprocess.call(['./LOCAL_rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
+
 	return output
