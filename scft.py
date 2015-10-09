851cae53fcac10f0de7d695e247dc568aa915e21
mladen5000 <mladenrasic@wireless-s1-no-150-28-191.uchicago.edu>
2015-10-09 15:01:23 -0500

    added custom rq-dash

--- scft.py
+++ scft.py
@@ -4,13 +4,14 @@ import os
 
 def hello_world():
 
-
 	#These 3 lines are for remote
+	"""
 	os.chdir('/app/SCFT_real')
 	subprocess.call(['make'])
 	output = subprocess.call(['./rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
+	"""
 
 	# This line is for local only
-	#output = subprocess.call(['./LOCAL_rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
+	output = subprocess.call(['./LOCAL_rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
 
 	return output
