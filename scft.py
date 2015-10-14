4fc262f56fb34a766d9e1f7567f1f13a36f373e6
mladen5000 <mladenrasic@wireless-s1-no-150-26-100.uchicago.edu>
2015-10-14 16:03:30 -0500

    readded scft,

--- scft.py
+++ scft.py
@@ -8,26 +8,16 @@ def hello_world():
 
 	if redis_url == 'redis://localhost:6379':
 		# This line is for local only
-		"""
 		output = subprocess.call(['./LOCAL_rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
 		
-		"""
-		var = 10 + 10
-		output = str(var)
-		return output
 	
 	else:
-		print 'not local'
-		print ''
-		print ''
 
 		#These 3 lines are for remote
-		"""
 		os.chdir('/app/SCFT_real')
 		subprocess.call(['make'])
 		output = subprocess.call(['./rscft','37','3','6','3','3','outfile1','outfile2','infile','1.78'])
-		"""
-		output = 10 + 10
+
 		output = str(output)
 
 
